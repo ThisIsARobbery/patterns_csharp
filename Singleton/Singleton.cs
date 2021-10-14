4c8c3885ff3f01bb76de9cb89fa16618b83839f2
Gordey.Babich <Gordey.Babich@x5.ru>
2021-10-15 00:38:05 +0300

    Added simplest singleton implementation

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.getInstance();
+            Singleton s2 = Singleton.getInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Dumbest version of singleton works!");
+            }
+            else
+            {
+                Console.WriteLine("Nothing works. Pity...");
+            }
+        }
+    }
+}
