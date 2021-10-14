87f7e726b14b4b11105cdeb83fa59a5ec378ef1a
Gordey.Babich <Gordey.Babich@x5.ru>
2021-10-14 20:16:26 +0300

    Added Prototype pattern example usage

--- Prototype/Prototype.cs
+++ Prototype/Prototype.cs
@@ -1,38 +1,13 @@
 ﻿using System;
 
-namespace PatternsChsarp.Prototype
+namespace Prototype
 {
-    public interface Prototype
-    {
-        public 
-    }
     public class Person
     {
-        private int _age;
-        private DateTime _birthDate;
-        private string _name;
-        private IdInfo _idInfo;
-
-        public int Age
-        {
-            get { return _age; }
-            set { _age = value; }
-        }
-        public DateTime BirthDate
-        {
-            get { return _birthDate; }
-            set { _birthDate = value; }
-        }
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
-        public IdInfo Id
-        {
-            get { return _idInfo; }
-            set { _idInfo = value; }
-        }
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo Id;
 
         public Person ShallowCopy()
         {
@@ -42,19 +17,62 @@ namespace PatternsChsarp.Prototype
         public Person DeepCopy()
         {
             Person clone = (Person) this.MemberwiseClone();
-            clone.Id = new IdInfo(IdInfo.IdNumber);
-            clone.Name = String.Copy(this._name)
+            clone.Id = new IdInfo(Id.IdNumber);
+            clone.Name = String.Copy(Name);
+            return clone;
         }
     }
 
     public class IdInfo
     {
-        private int _id;
-        public int IdNumber
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
         {
-            get { return _id; }
-            set { _id = value; }
+            this.IdNumber = idNumber;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Age = 42;
+            p1.BirthDate = Convert.ToDateTime("1977-01-01");
+            p1.Name = "jack daniels";
+
+            p1.Id = new IdInfo(666);
+
+            Person p2 = p1.ShallowCopy();
+            Person p3 = p1.DeepCopy();
+            
+            Console.WriteLine("Original values of p1, p2, p3:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values:");
+            DisplayValues(p3);
+            
+            p1.Age = 32;
+            p1.BirthDate = Convert.ToDateTime("1900-01-01");
+            p1.Name = "Frank";
+            p1.Id.IdNumber = 7878;
+            Console.WriteLine("\nValues of p1, p2 and p3 after changes to p1:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values (reference values have changed):");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values (everything was kept the same):");
+            DisplayValues(p3);
         }
 
+        public static void DisplayValues(Person p)
+        {
+            Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
+                p.Name, p.Age, p.BirthDate);
+            Console.WriteLine("      ID#: {0:d}", p.Id.IdNumber);
+        }
     }
 }
